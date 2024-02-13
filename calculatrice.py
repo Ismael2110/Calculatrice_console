cfddc38b2baeb1393491bad18677c407263cbdfc
COMPAORE Ismael <ismaelcompaore08@gmail.com>
2024-02-13 12:04:18 +0100

    premier commit

---
+++ calculatrice.py
@@ -0,0 +1,20 @@
+import math
+
+def addition(a, b):
+  "Fonction pour l'addition"
+  return a + b
+
+def soustraction(a, b):
+  "Fonction pour la soustraction"
+  return a - b
+
+def multiplication(a, b):
+  "Fonction pour la multiplication"
+  return a * b
+
+def division(a, b):
+  "Fonction pour la division"
+  return a / b
+
+
+
