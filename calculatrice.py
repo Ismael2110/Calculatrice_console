ba6a168fe20604e764dc93f1f39cff42981f2320
COMPAORE Ismael <ismaelcompaore08@gmail.com>
2024-02-13 12:33:16 +0100

    troisieme commit

--- calculatrice.py
+++ calculatrice.py
@@ -28,4 +28,26 @@ def main():
     if operation == "q":
       break
 
+# Demander à l'utilisateur de saisir les nombres
+    nombre_1 = float(input("Entrez le premier nombre: "))
+    nombre_2 = float(input("Entrez le deuxième nombre: "))
 
+    # Déterminer l'opération et afficher le résultat
+    if operation == "+":
+      resultat = addition(nombre_1, nombre_2)
+      print(f"{nombre_1} + {nombre_2} = {resultat}")
+    elif operation == "-":
+      resultat = soustraction(nombre_1, nombre_2)
+      print(f"{nombre_1} - {nombre_2} = {resultat}")
+    elif operation == "*":
+      resultat = multiplication(nombre_1, nombre_2)
+      print(f"{nombre_1} * {nombre_2} = {resultat}")
+    elif operation == "/":
+      resultat = division(nombre_1, nombre_2)
+      print(f"{nombre_1} / {nombre_2} = {resultat}")
+    else:
+      print("Opération invalide!")
+
+# Appeler la fonction principale
+if __name__ == "__main__":
+  main()
