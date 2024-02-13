afa186d4750a447fd97c2ec3586cf2546a583808
COMPAORE Ismael <ismaelcompaore08@gmail.com>
2024-02-13 12:26:56 +0100

    second commit

--- calculatrice.py
+++ calculatrice.py
@@ -15,6 +15,17 @@ def multiplication(a, b):
 def division(a, b):
   "Fonction pour la division"
   return a / b
+def main():
+  "Fonction principale"
+  # Afficher un message d'accueil
+  print("Calculatrice Console Python")
 
+  while True:
+    # Demander à l'utilisateur de choisir une opération
+    operation = input("Choisissez une opération (+, -, *, /, q pour quitter): ")
+
+    # Quitter l'application si l'utilisateur saisit "q"
+    if operation == "q":
+      break
 
 
